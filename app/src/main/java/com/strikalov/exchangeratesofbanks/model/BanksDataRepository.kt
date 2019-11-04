bc1e75cba7bb720aecf7607480f3208961e9906b
Alexander Strikalov <strikalov.aleksandr@yandex.ru>
2019-11-04 16:39:59 +0300

    add work with api

---
+++ app/src/main/java/com/strikalov/exchangeratesofbanks/model/BanksDataRepository.kt
@@ -0,0 +1,22 @@
+package com.strikalov.exchangeratesofbanks.model
+
+import com.strikalov.exchangeratesofbanks.system.SchedulersProvider
+import javax.inject.Inject
+
+class BanksDataRepository @Inject constructor(
+    private val api: Api,
+    private val schedulers: SchedulersProvider
+){
+
+    fun getExchangeRates() = api.getExchangeRates()
+        .subscribeOn(schedulers.io())
+        .observeOn(schedulers.ui())
+
+    fun getBankInfo(idBank: Int) = api.getBankInfo(idBank)
+        .subscribeOn(schedulers.io())
+        .observeOn(schedulers.ui())
+
+    fun getBankOffices(idBank: Int) = api.getBankOffices(idBank)
+        .subscribeOn(schedulers.io())
+        .observeOn(schedulers.ui())
+}
