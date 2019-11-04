6f7f3f084a7dd6d095b7bb9191cdc394c818adaa
Alexander Strikalov <strikalov.aleksandr@yandex.ru>
2019-11-04 16:59:22 +0300

    add ciceroni navigation

--- app/src/main/java/com/strikalov/exchangeratesofbanks/ui/MainActivity.kt
+++ app/src/main/java/com/strikalov/exchangeratesofbanks/ui/MainActivity.kt
@@ -1,21 +1,66 @@
 package com.strikalov.exchangeratesofbanks.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.strikalov.exchangeratesofbanks.R
 import com.strikalov.exchangeratesofbanks.di.DI
-import com.strikalov.exchangeratesofbanks.model.BanksDataRepository
-import kotlinx.android.synthetic.main.activity_main.*
 import moxy.MvpAppCompatActivity
-import timber.log.Timber
+import ru.terrakok.cicerone.Navigator
+import ru.terrakok.cicerone.NavigatorHolder
+import ru.terrakok.cicerone.Router
+import ru.terrakok.cicerone.android.support.SupportAppNavigator
+import ru.terrakok.cicerone.commands.Command
 import toothpick.Toothpick
 import javax.inject.Inject
 
 class MainActivity : MvpAppCompatActivity() {
+
+    @Inject
+    lateinit var router: Router
+
+    @Inject
+    lateinit var navigatorHolder: NavigatorHolder
+
+    private val navigator: Navigator =
+        object : SupportAppNavigator(this, supportFragmentManager, R.id.container) {
+            override fun setupFragmentTransaction(
+                command: Command?,
+                currentFragment: Fragment?,
+                nextFragment: Fragment?,
+                fragmentTransaction: FragmentTransaction
+            ) {
+                //fix incorrect order lifecycle callback of MainFlowFragment
+                fragmentTransaction.setReorderingAllowed(true)
+            }
+
+            override fun applyCommands(commands: Array<out Command>?) {
+                if (Thread.currentThread() != Looper.getMainLooper().thread) {
+                    runOnUiThread {
+                        super.applyCommands(commands)
+                    }
+                } else {
+                    super.applyCommands(commands)
+                }
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Toothpick.inject(this, Toothpick.openScope(DI.APP_SCOPE))
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        navigatorHolder.removeNavigator()
+        super.onPause()
+    }
+
 }
